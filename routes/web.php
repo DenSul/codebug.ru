72c46b0fea3cabf9f93b6b82c5c40712b3635389
Sultanov Denis <sultanden@gmail.com>
2019-06-09 19:12:26 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Laravel\Lumen\Routing\Router;
 
-/** @var Router $router */
-$router->group(['namespace' => 'Blog'], function (Router $app)  {
+/* @var Router $router */
+$router->group(['namespace' => 'Blog'], function (Router $app) {
     $app->get('/', ['as' => 'index', 'uses' => 'IndexController@show']);
 });
