dc928e2cf5078ecdace8f17081a0327dcdf0d856
Mio <sultanden@gmail.com>
2019-07-10 22:04:36 +0500

    fun template

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,88 +4,33 @@
 @endsection
 
 @section('main_content')
-    <noindex>
-        <div class="container">
-            <div class="jumbotron">
-                <h1 class="display-3">Подумай, уходи!</h1>
-                <div class="lead">
-                    <p>
-                        <span class="text-danger">Автор</span> данного блога упоротый, безграмотный и вообще
-                        <span class="text-danger">долбоеб</span>!
-                    </p>
-                    <a href="#" class="btn btn-danger float-left">Ну нахуй</a>
-                    <a href="#" class="btn btn-success float-right">Хорошо</a>
-                </div>
-            </div>
-        </div>
-    </noindex>
     <div class="container">
-        <div class="row">
-            <!-- Blog Entries Column -->
-            <div class="col-md-12">
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                    <div class="card-body">
-                        <a class="post-title" href="#">Хуйня из под коня!</a>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-                            aliquid
-                            atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                            voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a href="#" class="btn btn-danger float-left">&larr; Пошел нахуй</a>
-                        <a href="#" class="btn btn-primary float-right">Буду читать &rarr;</a>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Posted on January 1, 2017 by
-                        <a href="#">Start Bootstrap</a>
-                    </div>
-                </div>
-
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                    <div class="card-body">
-                        <h2 class="card-title">Post Title</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-                            aliquid
-                            atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                            voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Posted on January 1, 2017 by
-                        <a href="#">Start Bootstrap</a>
-                    </div>
-                </div>
-
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                    <div class="card-body">
-                        <h2 class="card-title">Post Title</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-                            aliquid
-                            atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                            voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                    </div>
-                    <div class="card-footer text-muted">
-                        Posted on January 1, 2017 by
-                        <a href="#">Start Bootstrap</a>
-                    </div>
-                </div>
-
-                <!-- Pagination -->
-                <ul class="pagination justify-content-center mb-4">
-                    <li class="page-item">
-                        <a class="page-link" href="#">&larr; Older</a>
-                    </li>
-                    <li class="page-item disabled">
-                        <a class="page-link" href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
-            </div>
+        <div class="last-post">
+            <a href="#">
+                <img src="{{url('images/banners/last_post.jpg')}}" alt="" class="img-fluid" />
+                <span class="last-post-content">
+                    <span class="last-post-content-category">
+                        sass/less/css
+                    </span>
+                    <span class="last-post-content-title">
+                        Учимся писать стили на Sass/Less/Css
+                    </span>
+                    <span class="last-post-content-date">
+                        10 Июня, 2019
+                    </span>
+                </span>
+            </a>
         </div>
+    </div>
+    {{--<div class="container">--}}
+        {{--<div class="last-post">--}}
+            {{--<div class="last-post__content">--}}
+                {{--<h3>sass/less/css</h3>--}}
+                {{--<h1>Учимся писать стили на Sass/Less/Css</h1>--}}
+                {{--<h4>10 Июня, 2019</h4>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 @endsection
 
 @section('inline_scripts')
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                         loader: 'style-loader'
                     },
                     {
-                        loader: 'css-loader'
+                        loader: 'css-loader', options: {url: false}
                     },
                     {
                         loader: 'postcss-loader',
