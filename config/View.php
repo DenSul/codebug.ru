9fe1b819076f84b6de3650bb41c9c7f84be48bdb
Mio <sultanden@gmail.com>
2019-07-30 04:28:47 +0500

    fixed tests

--- config/View.php
+++ config/View.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types = 1);
+
 $theme = 'default';
 
 return [
     'paths'    => [
         resource_path(sprintf('themes/%s/views', $theme)),
     ],
-    'compiled' => '/app/storage/framework/views',
+    'compiled' => storage_path('framework/views'),
 ];
